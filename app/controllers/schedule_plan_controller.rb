42a8abf6d0185d14bb53ac9a7a9c2d80cc2f77f3
dcastro <b.boy.souljah@gmail.com>
2011-11-02 16:04:06 +0000

    schedule plan create bug fixed

--- app/controllers/schedule_plan_controller.rb
+++ app/controllers/schedule_plan_controller.rb
@@ -1,19 +1,19 @@
 class SchedulePlanController < ApplicationController
   
   # curl -X POST -d "{\"doctor_id\":22,\"days\":[{\"weekday\":2, \"start\":600, \"end\":720}, {\"weekday\":4, \"start\":840, \"end\":1140}]}" localhost:3000/schedule_plan/create
-  # curl -X POST -d "{\"doctor_id\":22,\"days\":[{\"weekday\":2, \"start\":600, \"end\":720}, {\"weekday\":6, \"start\":840, \"end\":1140}], \"start_date\":\"08-10-2011\"}" localhost:3000/schedule_plan/create
+  # curl -X POST -d "{\"doctor_id\":23,\"days\":[{\"weekday\":2, \"start\":600, \"end\":720}, {\"weekday\":6, \"start\":840, \"end\":1140}], \"start_date\":\"08-10-2011\"}" localhost:3000/schedule_plan/create
 
   def create
     
     @json = ActiveSupport::JSON.decode(request.body.read)
     
     @sch = SchedulePlan.new :doctor => Doctor.find(session[:id]), #@json["doctor_id"]),
-                            :start_date => @json["start_date"]                            
-    
+                            :start_date => @json["start_date"]       
+                            
     @json["days"].each do |d|
-      @sch.workdays << Workday.new( :weekday => d["weekday"],
+      @sch.workdays.build :weekday => d["weekday"], #workdays << Workday.new( :weekday => d["weekday"],
                                    :start => d["start"],
-                                   :end => d["end"] )
+                                   :end => d["end"] 
     end
     
     begin
